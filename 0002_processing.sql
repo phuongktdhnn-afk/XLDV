8c0eec1a3a6ffe84cb3ac837d6b5d5a1c6c85ad0
phuongktdhnn-afk <phuongktdhnn@gmail.com>
2026-09-03 17:46:58 +0700

    Delete 0002_processing.sql

--- 0002_processing.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE students ADD COLUMN processing_status TEXT NOT NULL DEFAULT 'OPEN';
-ALTER TABLE students ADD COLUMN processed_by TEXT;
-ALTER TABLE students ADD COLUMN processed_at TEXT;
-ALTER TABLE students ADD COLUMN resolution_note TEXT DEFAULT '';
-CREATE INDEX IF NOT EXISTS idx_students_processing_status ON students(processing_status);
